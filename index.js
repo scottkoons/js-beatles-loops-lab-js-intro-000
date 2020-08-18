ed8eb76fe25fb4c090a9706bc8e32b862807a6ca
communal-documentation-0347 <scottkoons@gmail.com>
2020-08-18 19:50:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+let musiciansPlay = [];
+
+function createArr(name, play) {
+    for (let i = 0; i < musicians.length; i++) {
+        musiciansPlay.push(`${name[i]} plays ${play[i]}`);
+    };
+    return musiciansPlay;
+};
